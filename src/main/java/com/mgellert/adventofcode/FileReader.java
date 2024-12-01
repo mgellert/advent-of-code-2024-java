d72ee54f4a4609ebab5cb853deaeed6a74da5c14
gellertm <mgellert@users.noreply.github.com>
2024-12-01 10:43:24 +0100

    Added exist method to FileReader.

--- src/main/java/com/mgellert/adventofcode/FileReader.java
+++ src/main/java/com/mgellert/adventofcode/FileReader.java
@@ -10,7 +10,7 @@ public class FileReader {
 
     public static List<String> readLines(String day) {
         try {
-            return Files.readAllLines(Path.of("src/main/resources/inputs", day)).stream()
+            return Files.readAllLines(getPath(day)).stream()
                     .filter(Objects::nonNull)
                     .toList();
         } catch (IOException e) {
@@ -20,5 +20,13 @@ public class FileReader {
 
     public static List<Long> readLinesAsLong(String day) {
         return readLines(day).stream().map(Long::parseLong).toList();
+    }
+
+    public static boolean exists(String day) {
+        return Files.exists(getPath(day));
+    }
+
+    private static Path getPath(String day) {
+        return Path.of("src/main/resources/inputs", day);
     }
 }
--- src/test/java/com/mgellert/adventofcode/FileReaderTest.java
+++ src/test/java/com/mgellert/adventofcode/FileReaderTest.java
@@ -5,13 +5,15 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileReaderTest {
 
     @Test
     void shouldReadLinesFromFile() {
-        List<String> testInput = FileReader.readLines("testinput");
-        assertEquals(3, testInput.size());
+        List<String> testInput = FileReader.readLines("day01");
+        assertEquals(1000, testInput.size());
     }
 
     @Test
@@ -19,5 +21,11 @@ class FileReaderTest {
         List<Long> testInput = FileReader.readLinesAsLong("testinput");
         Long sum = testInput.stream().mapToLong(a -> a).sum();
         assertEquals(6, sum);
+    }
+
+    @Test
+    void shouldTestExists() {
+        assertTrue(FileReader.exists("day01"));
+        assertFalse(FileReader.exists("foobar"));
     }
 }
